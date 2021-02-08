7b862a8728c6e2c502cf81d427e5fc15dca6275b
ClintFix <clint.fix@gmail.com>
2021-02-08 14:11:01 -0700

    main content added

--- js/index.js
+++ js/index.js
@@ -36,21 +36,49 @@ const siteContent = {
     "copyright" : "Copyright Great Idea! 2018"
   },
 };
+// // Example: Update the img src for the logo
+// let logo = document.getElementById("logo-img");
+// logo.setAttribute('src', siteContent["nav"]["img-src"])
 
-// Example: Update the img src for the logo
-let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
 
+//Creating Selectors
 const navLinks = document.querySelectorAll('nav a'); //New array like object that includes all nav links
+const logoImg = document.querySelector('#logo-img');
+const ctaHeaderText = document.querySelector('.cta-text h1');
+const ctaButtonText = document.querySelector('.cta-text button');
+const ctaImgSrc = document.querySelector('#cta-img');
+const mainContentH4 = document.querySelectorAll('h4');
+const mainContentP = document.querySelectorAll('.main-content .text-content p');
+const middleImg = document.querySelector('#middle-img');
+
+//Update Nav Links
 for(let i = 0; i < navLinks.length; i++){ //for loop to assign text to the anchor tags in nav and iterate through site content object
   navLinks[i].textContent = siteContent.nav[`nav-item-${i+1}`];
 }
 
 //add logo image source to header
-document.querySelector('#logo-img').src = siteContent.nav["img-src"];
+logoImg.src = siteContent.nav["img-src"];
 
 // CTA attributes
-document.querySelector('.cta-text h1').textContent = siteContent.cta["h1"]; //add text to h1 header
-document.querySelector('.cta-text button').textContent = siteContent.cta["button"]; //add text to button
-document.querySelector('#cta-img').src = siteContent.cta["img-src"];
+ctaHeaderText.textContent = siteContent.cta["h1"]; //add text to h1 header
+ctaButtonText.textContent = siteContent.cta["button"]; //add text to button
+ctaImgSrc.src = siteContent.cta["img-src"];
+
+// Main Content Headers
+mainContentH4[0].textContent = siteContent["main-content"]["features-h4"];
+mainContentH4[1].textContent = siteContent["main-content"]["about-h4"];
+mainContentH4[2].textContent = siteContent["main-content"]["services-h4"];
+mainContentH4[3].textContent = siteContent["main-content"]["product-h4"];
+mainContentH4[4].textContent = siteContent["main-content"]["vision-h4"];
+
+// Main Content Paragraphs
+mainContentP[0].textContent = siteContent['main-content']['features-content'];
+mainContentP[1].textContent = siteContent['main-content']['about-content'];
+mainContentP[2].textContent = siteContent['main-content']['services-content'];
+mainContentP[3].textContent = siteContent['main-content']['product-content'];
+mainContentP[4].textContent = siteContent['main-content']['vision-content'];
+
+//middle image
+middleImg.src = siteContent['main-content']['middle-img-src'];
+
 
